a9ca7539e8cb2791415e0daab3abc7a90c5dff59
SHUHEI HIYA <syuhei176@gmail.com>
2019-05-10 07:23:17 +0900

    Add mock contracts

---
+++ contracts/PlasmaChain.vy
@@ -0,0 +1,6 @@
+commitment_address: public(address)
+
+# @dev Constructor
+@public
+def __init__(_commitment_address: address):
+  self.commitment_address = _commitment_address
--- migrations/2_deploy_contracts.js
+++ migrations/2_deploy_contracts.js
@@ -1,8 +1,19 @@
 const PredicateUtils = artifacts.require('PredicateUtils')
 const OwnershipPredicate = artifacts.require('OwnershipPredicate')
+const CommitmentChain = artifacts.require('CommitmentChain')
+const PlasmaChain = artifacts.require('PlasmaChain')
 
 module.exports = function(deployer) {
   deployer
     .deploy(PredicateUtils)
-    .then(() => deployer.deploy(OwnershipPredicate, PredicateUtils.address))
+    .then(() => deployer.deploy(CommitmentChain))
+    .then(() => deployer.deploy(PlasmaChain, CommitmentChain.address))
+    .then(() =>
+      deployer.deploy(
+        OwnershipPredicate,
+        CommitmentChain.address,
+        PlasmaChain.address,
+        PredicateUtils.address
+      )
+    )
 }
